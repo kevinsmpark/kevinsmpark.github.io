2aa01c1fe06b0aebcf9787ca9f158e6d32d97220
Kevin Park <kevinsmpark@icloud.com>
2015-05-08 21:26:52 -0700

    2.1.0

--- home/js/app.js
+++ home/js/app.js
@@ -7,7 +7,7 @@
 /* config dom id (optional) + config particles params */
 particlesJS('particles-js', {
   particles: {
-    color: '#fff',
+    color: '#000',
     color_random: false,
     shape: 'circle', // "circle", "edge" or "triangle"
     opacity: {
@@ -25,7 +25,7 @@ particlesJS('particles-js', {
     line_linked: {
       enable_auto: true,
       distance: 100,
-      color: '#fff',
+      color: '#000',
       opacity: 1,
       width: 1,
       condensed_mode: {
